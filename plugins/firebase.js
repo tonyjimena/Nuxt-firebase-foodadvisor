1fa0fad5d842345c217e5a49208287108265705c
tony j <59872884+tonyjimena@users.noreply.github.com>
2020-04-30 18:50:18 +0200

    Update firebase.js

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/firebase-auth'
 
 if (!firebase.apps.length) {
   const firebaseConfig = {
-    apiKey: "AIzaSyChG5I3FYs4ZPWMJk8sbmeVaUne_yEq388",
-    authDomain: "foodadvisor-9790e.firebaseapp.com",
-    databaseURL: "https://foodadvisor-9790e.firebaseio.com",
-    projectId: "foodadvisor-9790e",
-    storageBucket: "foodadvisor-9790e.appspot.com",
-    messagingSenderId: "434079795585",
-    appId: "1:434079795585:web:ef02b9cebc434283d78080"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 
   }
 
